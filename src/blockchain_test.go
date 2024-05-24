2a34349c29a8cc862617274c9c7710ebf93d5784
de-wan <stephen.wandie@churpy.co>
2024-05-24 16:47:27 +0300

    tests: Initialized tests

--- src/blockchain_test.go
+++ src/blockchain_test.go
@@ -1,1 +1,55 @@
 package src
+
+import "testing"
+
+func TestCreateBlock(t *testing.T) {
+
+}
+
+func TestCreateWallet(t *testing.T) {
+
+}
+
+func TestSelectNextValidator(t *testing.T) {
+
+}
+
+func TestValidate(t *testing.T) {
+
+}
+
+func TestGetLastBlock(t *testing.T) {
+
+}
+
+func TestCreateTransaction(t *testing.T) {
+
+}
+
+func TestSignTransaction(t *testing.T) {
+
+}
+
+func TestReadTransaction(t *testing.T) {
+
+}
+
+func TestCreateSmartContract(t *testing.T) {
+
+}
+
+func TestRegisterNode(t *testing.T) {
+
+}
+
+func TestValidateNode(t *testing.T) {
+
+}
+
+func TestMalformedNode(t *testing.T) {
+
+}
+
+func TestNodeIdempotency(t *testing.T) {
+
+}
